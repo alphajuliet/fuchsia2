dbd8e7f6028154c8c024bb02529f5508a51d4093
alphajuliet <andrew@alphajuliet.com>
2024-05-07 21:31:38 +1000

    Update version

--- src/ts/fuchsia.ts
+++ src/ts/fuchsia.ts
@@ -8,8 +8,8 @@
 const Info = {
 	name: "fuchsia",
 	author: "AndrewJ", 
-	version: "2.1.1",
-	date: "2024-04-22",
+	version: "2.2.0",
+	date: "2024-05-07",
 	appendTo: function(tagName) {
 		const src = document.getElementById(tagName);
 		const title = document.createElement('span');
